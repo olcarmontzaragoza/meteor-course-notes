d63c9667bdbd6e7439d7360cc3b0dc9402cd3e4a
Oliver <olcarmontzaragoza@gmail.com>
2018-09-13 09:58:37 +0200

    Init commit

---
+++ imports/routes/routes.js
@@ -0,0 +1,56 @@
+import { Meteor } from 'meteor/meteor';
+import React from 'react';
+
+import Signup from '../ui/Signup';
+import NotFound from '../ui/NotFound';
+import Dashboard from '../ui/Dashboard';
+import Login from '../ui/Login';
+
+import { Router, Route, browserHistory, Switch, Redirect } from 'react-router';
+
+import createBrowserHistory from 'history/createBrowserHistory';
+
+browserHistory = createBrowserHistory();
+
+const unauthenticatedPages = ['/', '/signup'];
+const authenticatedPages = ['/dashboard'];
+
+const onEnterPublicPage = (Component) => {
+    if (Meteor.userId()) {
+        return <Redirect to="/dashboard" />;
+    } else {
+        return <Component />;
+    }
+};
+
+const onEnterPrivatePage = (Component) => {
+    if (!Meteor.userId()) {
+        return <Redirect to="/" />;
+    } else {
+        return <Component />;
+    }
+};
+
+export const onAuthChange = (isAuthenticated) => {
+const pathname = browserHistory.location.pathname;
+const isUnauthenticatedPage = unauthenticatedPages.includes(pathname);
+const isAuthenticatedPage = authenticatedPages.includes(pathname);
+
+if (isAuthenticated && isUnauthenticatedPage) {
+browserHistory.replace('/dashboard');
+}
+if (!isAuthenticated && isAuthenticatedPage) {
+browserHistory.replace('/');
+}
+};
+
+export const routes = (
+    <Router history={browserHistory}>
+        <Switch>
+          <Route exact path="/" render={() => onEnterPublicPage(Login)} />
+          <Route path="/dashboard"  render={() => onEnterPrivatePage(Dashboard)}  />
+          <Route path="/signup" render={() => onEnterPublicPage(Signup)}  />
+          <Route path="*" component={NotFound}  />
+        </Switch>
+    </Router>
+);
