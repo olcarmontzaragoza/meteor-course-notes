4d63de51afe32d39a005258844a876d60726fcce
Oliver <olcarmontzaragoza@gmail.com>
2018-10-04 07:56:19 +0200

    Style editor component

--- imports/ui/Editor.js
+++ imports/ui/Editor.js
@@ -43,15 +43,17 @@ render() {
   if (this.props.note) {
   return (
   <div className="editor">
-  <input value={this.state.title} placeholder='Untitled Note' onChange={this.handleTitleChange.bind(this)}/>
-  <textarea value={this.state.body} placeholder='Your Note Here' onChange={this.handleBodyChange.bind(this)}></textarea>
-  <button onClick={() => { Meteor.call('note.remove'); }}>Delete Note</button>
+  <input className="editor__title" value={this.state.title} placeholder='Untitled Note' onChange={this.handleTitleChange.bind(this)}/>
+  <textarea className="editor__body" value={this.state.body} placeholder='Your Note Here' onChange={this.handleBodyChange.bind(this)}></textarea>
+  <div>
+  <button className="button button-secondary" onClick={() => { Meteor.call('note.remove'); }}>Delete Note</button>
+  </div>
   </div>
   );
 } else {
   return (
   <div className="editor">
-  <p>{ this.props.selectedNoteId ? 'Note not found.' : 'Pick or create a note to get started.'}</p>
+  <p className="editor__message">{ this.props.selectedNoteId ? 'Note not found.' : 'Pick or create a note to get started.'}</p>
   </div>
   );
   }
